c49983bfdd32f6e44e486c5239407a8abf84e8fe
Amir Hossein Saberi <amirhosseinsaberi1376@gmail.com>
2023-11-21 11:04:20 +0330

    Investment App

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,54 @@
+const UserInput = ({ userInput, handleChange }) => {
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label htmlFor="">Initial Investment</label>
+          <input
+            type="number"
+            value={userInput.initialInvestment}
+            onChange={(event) =>
+              handleChange("initialInvestment", event.target.value)
+            }
+            required
+          />
+        </p>
+        <p>
+          <label htmlFor="">Annual Investment</label>
+          <input
+            type="number"
+            value={userInput.annualInvestment}
+            onChange={(event) =>
+              handleChange("annualInvestment", event.target.value)
+            }
+            required
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label htmlFor="">Expected Return</label>
+          <input
+            type="number"
+            value={userInput.expectedReturn}
+            onChange={(event) =>
+              handleChange("expectedReturn", event.target.value)
+            }
+            required
+          />
+        </p>
+        <p>
+          <label htmlFor="">Duration</label>
+          <input
+            type="number"
+            value={userInput.duration}
+            onChange={(event) => handleChange("duration", event.target.value)}
+            required
+          />
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default UserInput;
